4a84b6efffe086afb2e5d6fb6424ce2a5700df63
6thSence <d.pushkarskaya.pr@gmail.com>
2016-07-29 18:23:15 +0600

    weakMap

---
+++ weakMap/scripts/main.js
@@ -0,0 +1,29 @@
+let animals = [
+  { animal: 'tiger' },
+  { animal: 'elephant' },
+  { animal: 'giraffe' },
+];
+
+let weakMap = new WeakMap([
+    [animals[0], 1],
+    [animals[1], 2],
+    [animals[2], 3]]);
+
+console.log( weakMap.get(animals[0]) ); // 1
+console.log( weakMap ); // 1
+
+animals.splice(0, 1); // tiger больше не находится в массиве
+
+console.log(animals);
+console.log(weakMap.has(animals[0])); // в какой-то неизвестный нам момент времени сборщик мусора удалит этот элемент
+
+// weakMap теперь содержит только 2 элемента
+
+animals.splice(0, 1); // elephant больше не находится в массиве
+
+console.log(weakMap.has(animals[1])); // в какой-то неизвестный нам момент времени сборщик мусора удалит этот элемент
+
+// weakMap теперь содержит только 1 элемент
+
+console.log(animals);
+console.log(weakMap.has(animals[2])); // в какой-то неизвестный нам момент времени сборщик мусора удалит этот элемент
